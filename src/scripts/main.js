a02113de5eeec2235ed0c2b3e923953df26dfe56
Davina Leong <davina.leong.sy@gmail.com>
2023-06-22 09:26:44 +0800

    Removed console logs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,12 +6,10 @@ const ariaExpandedAttr = `aria-expanded`
 const fieldsetEls = document.querySelectorAll(`fieldset`)
 
 if (fieldsetEls && fieldsetEls.length > 0) {
-    console.log("fieldsetEls.length", fieldsetEls.length)
     fieldsetEls.forEach(function (fieldsetEl) {
         const legendEl = fieldsetEl.querySelector(`legend`)
         legendEl.addEventListener(`click`, function (event) {
             const ariaExpanded = fieldsetEl.getAttribute(ariaExpandedAttr)
-            console.log(ariaExpanded)
             fieldsetEl.setAttribute(ariaExpandedAttr, "true")
             if (ariaExpanded === "true") {
                 fieldsetEl.removeAttribute(ariaExpandedAttr)
