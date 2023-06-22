0bb4755657ef8f8ff8b6fe2950cb640eab316dce
Davina Leong <davina.leong.sy@gmail.com>
2023-06-22 09:25:55 +0800

    Able to collapse fieldsets

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 console.log(`main.js loaded`)
+
+const dataElementAttr = `data-element`
+const ariaExpandedAttr = `aria-expanded`
+
+const fieldsetEls = document.querySelectorAll(`fieldset`)
+
+if (fieldsetEls && fieldsetEls.length > 0) {
+    console.log("fieldsetEls.length", fieldsetEls.length)
+    fieldsetEls.forEach(function (fieldsetEl) {
+        const legendEl = fieldsetEl.querySelector(`legend`)
+        legendEl.addEventListener(`click`, function (event) {
+            const ariaExpanded = fieldsetEl.getAttribute(ariaExpandedAttr)
+            console.log(ariaExpanded)
+            fieldsetEl.setAttribute(ariaExpandedAttr, "true")
+            if (ariaExpanded === "true") {
+                fieldsetEl.removeAttribute(ariaExpandedAttr)
+            }
+        })
+    })
+}
