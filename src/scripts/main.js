e062857878e887bfacf85028e3da3c8957f0c9d1
Davina Leong <leong.shi.yun@gmail.com>
2023-06-25 10:50:59 +0800

    Done with menu component

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,51 @@
 console.log(`main.js loaded`)
 
+// Attributes
 const dataElementAttr = `data-element`
 const ariaExpandedAttr = `aria-expanded`
 
+// Elements
 const fieldsetEls = document.querySelectorAll(`fieldset`)
+const menuEls = document.querySelectorAll(`.menu`)
 
+// Logic
 if (fieldsetEls && fieldsetEls.length > 0) {
-    fieldsetEls.forEach(function (fieldsetEl) {
-        const legendEl = fieldsetEl.querySelector(`legend`)
-        legendEl.addEventListener(`click`, function (event) {
-            const ariaExpanded = fieldsetEl.getAttribute(ariaExpandedAttr)
-            fieldsetEl.setAttribute(ariaExpandedAttr, "true")
-            if (ariaExpanded === "true") {
-                fieldsetEl.removeAttribute(ariaExpandedAttr)
-            }
-        })
+  fieldsetEls.forEach(function (fieldsetEl) {
+    // On load, expand all fieldsets
+    fieldsetEl.setAttribute(ariaExpandedAttr, `true`)
+
+    const legendEl = fieldsetEl.querySelector(`legend`)
+    legendEl.addEventListener(`click`, function (event) {
+      event.preventDefault()
+      toggleElement(fieldsetEl)
     })
+  })
+}
+
+if (menuEls && menuEls.length > 0) {
+  menuEls.forEach(function (menuEl) {
+    const menuItemEls = menuEl.querySelectorAll(`.menu-item`)
+    menuItemEls.forEach(function (menuItemEl, index) {
+      // On load, expand the first menu item and collapse the rest
+      menuItemEl.setAttribute(ariaExpandedAttr, `true`)
+      if (index > 0) {
+        menuItemEl.removeAttribute(ariaExpandedAttr)
+      }
+
+      const menuItemLabelEl = menuItemEl.querySelector(`.menu-item-label`)
+      menuItemLabelEl.addEventListener(`click`, function (event) {
+        event.preventDefault()
+        toggleElement(menuItemEl)
+      })
+    })
+  })
+}
+
+// Functions
+function toggleElement(element) {
+  const ariaExpanded = element.getAttribute(ariaExpandedAttr)
+  element.setAttribute(ariaExpandedAttr, `true`)
+  if (ariaExpanded === `true`) {
+    element.removeAttribute(ariaExpandedAttr)
+  }
 }
